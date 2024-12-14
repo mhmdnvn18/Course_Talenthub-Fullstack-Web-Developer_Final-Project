d4c70b80e571288c67336abb6f3de23623f80b93
anop <81241987+mhmdnvn18@users.noreply.github.com>
2024-12-15 00:11:48 +0700

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   // Fungsi untuk mendapatkan data warung
   async function getData() {
-    const url = "https://jungle-nice-snowman.glitch.me/Warung";
+    const url = "https://actually-infrequent-dugout.glitch.me/warung";
     try {
       const response = await fetch(url);
       if (!response.ok) {
